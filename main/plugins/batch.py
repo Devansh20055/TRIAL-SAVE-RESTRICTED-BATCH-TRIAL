96fadb353ec417ce0731e6c614f9f145e843f6b7
Harshit Shrivastav <87630299+Itzharshit@users.noreply.github.com>
2022-02-20 18:30:02 +0530

    Update batch.py

--- main/plugins/batch.py
+++ main/plugins/batch.py
@@ -29,7 +29,9 @@ async def get_pvt_content(event, chat, id):
     msg = await userbot.get_messages(chat, ids=id)
     await event.client.send_message(event.chat_id, msg) 
     
-@Drone.on(events.NewMessage(incoming=True, pattern='/batch'))
+
+
+@bot.on(events.NewMessage(pattern="^/batch"))
 async def _batch(event):
     if not event.is_private:
         return
